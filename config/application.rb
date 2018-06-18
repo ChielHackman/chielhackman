9361fd605ff01cb74f7ca4c57205aeda70f1e662
Chiel <c.hackman@cevesvergeer.nl>
2018-06-18 22:02:50 +0200

    Fixed typo and added haml

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module Hackman
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.generator.stylesheets = false
-    config.generator.javascripts = false
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
